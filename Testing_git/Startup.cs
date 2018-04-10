9f3519ec88a3276b4c96d1cb93079c32808e0b2a
Pinal Bhadanee <pinalbhadanee@gmail.com>
2018-04-10 10:22:35 +0530

    bchghd

---
+++ Testing_git/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Testing_git.Startup))]
+namespace Testing_git
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);//gmdsmgdsklsdmvsdlm
+        }
+    }
+}
